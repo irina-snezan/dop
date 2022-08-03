3a5a5deb0a13d4512619717059e3bdaae2e28b13
irina-snezan <irina-snezan@tut.by>
2022-08-03 18:57:30 +0300

    1-getRequest+Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from './components/Button';
+
+type getType = {
+    'userId': number
+    'id': number
+    'title': string
+    'body': string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [get, setGet] = useState<Array<getType>>([])
+    const getRequestHandler = () => {
+       setGet([])
+    }
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => setGet(json))
+    }, [])
+
+    return (
+        <div className="App">
+            <Button nickName={'CleanPage'} callBack={getRequestHandler}/>
+            <p></p>
+            {get.map((el) => {
+                return (
+
+                    <li>
+                        <span>{el.id}</span>
+                        <span>{el.title}</span>
+                        <span>{el.body}</span>
+                    </li>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+type ButtonTypeProps={
+    nickName:string,
+    callBack:()=>void
+}
+
+export const Button=(props:ButtonTypeProps)=>{
+    const onClickHandler=()=>{
+        props.callBack()
+    }
+    return(
+        <button onClick={onClickHandler}>{props.nickName}</button>
+    )
+}
